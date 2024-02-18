aa48a3914ab02612ba10a335790fceecec1f2b14
Mizer <mucahitizer@gmail.com>
2024-02-18 17:50:15 +0300

    Recipe model changed, RecipeProperties, enums created created.

--- TarifDostuApi/Models/Recipe.cs
+++ TarifDostuApi/Models/Recipe.cs
@@ -4,24 +4,36 @@
     {
         public int Id { get; set; } // Tarifin benzersiz kimliği
         public string Name { get; set; } // Tarifin adı
-        public string Description { get; set; } // Tarifin açıklaması
-        public List<Ingredient> Ingredients { get; set; } // Tarifin malzemeleri listesi
+        public string ShortDescription { get; set; } // Tarifin açıklaması
+        public string BodyDescription { get; set; } // Tarifin açıklaması
         public string Instructions { get; set; } // Tarifin yapılış talimatları
-        public string DifficultyLevel { get; set; } // Tarifin zorluk seviyesi
-        public int ServingSize { get; set; } // Tarifin kaç kişilik olduğu
         public TimeSpan PreparationTime { get; set; } // Hazırlık süresi
         public TimeSpan CookingTime { get; set; } // Pişirme süresi
-        public string Cuisine { get; set; } // Tarifin ait olduğu mutfak
-        public string Course { get; set; } // Tarifin ait olduğu kurs
-        public string MealType { get; set; } // Tarifin öğün tipi
+        public List<Ingredient> Ingredients { get; set; } // Tarifin malzemeleri listesi
+        public List<RecipePropeties> Properties { get; set; } // Tarifin özelikleri RecipeProperties modeline bağlı veritabanında JSON olarak tutacak.
         public List<string> Tags { get; set; } // Tarife ilişkilendirilmiş etiketler
-        public string ImageUrl { get; set; } // Tarifin resim URL'si
-        public bool IsVegetarian { get; set; } // Vejetaryen mi?
+
+        public List<Station> Stations { get; set; } // Tarifin gerektirdiği pişirme istasyonları listesi
+    }
+
+    public class RecipePropeties
+    {
+        public List<MealType> MealType { get; set; } // Tarifin öğün tipi
+        public DifficultyLevel DifficultyLevel { get; set; } // Tarifin zorluk seviyesi
+        public List<ServingSize> ServingSize { get; set; } // Tarifin kaç kişilik olduğu
+        public List<Cuisine> Cuisine { get; set; } // Tarifin ait olduğu mutfak
+        public List<Course> Course { get; set; } // Tarifin ait olduğu kurs
+        public bool IsSpicy { get; set; } // Baharatlı mı?
+        public bool IsHealthy { get; set; } // Sağlıklı mı ?
+        public bool IsQuick { get; set; } // Hızlı mı?
         public bool IsVegan { get; set; } // Vegan mı?
         public bool IsGlutenFree { get; set; } // Gluten içermiyor mu?
         public bool IsDairyFree { get; set; } // Süt ürünü içermiyor mu?
+        public bool IsVegetarian { get; set; } // Vejetaryen mi?
         public bool IsCustom { get; set; } // Özel bir tarif mi yoksa genel bir tarif mi olduğunu belirtir
-        public List<Station> Stations { get; set; } // Tarifin gerektirdiği pişirme istasyonları listesi
+        public string MainImageUrl { get; set; } // Tarifin Ön resim URL'si
+        public List<string> ImageUrls { get; set; } // Tarifin resim URL'leri
+        public List<string> VideoUrls { get; set; } // Tarifin video URL'leri
     }
 
 }
---
+++ TarifDostuApi/Models/RecipeEnums.cs
@@ -0,0 +1,61 @@
+﻿namespace TarifDostuApi.Models
+{
+    public enum MealType
+    {
+        Breakfast,      // Kahvaltı
+        Lunch,          // Öğle Yemeği
+        Tea,            // Çay Saati
+        Dinner,         // Akşam Yemeği
+        Snack,          // Atıştırmalık
+        Other           // Diğer
+    }
+
+    public enum DifficultyLevel
+    {
+        VeryEasy,       // Çok Kolay
+        Easy,           // Kolay
+        Moderate,       // Orta
+        Intermediate,   // Orta Seviye
+        Challenging,    // Zor
+        Difficult,      // Çok Zor
+        VeryDifficult,  // Uzman
+        Expert          // Expert
+    }
+
+    public enum ServingSize
+    {
+        Small = 1,      // Küçük
+        Medium = 2,     // Orta
+        Large = 3,      // Büyük
+        ExtraLarge = 4  // Çok Büyük
+    }
+
+    public enum Cuisine
+    {
+        Turkish,        // Türk
+        Italian,        // İtalyan
+        French,         // Fransız
+        Chinese,        // Çin
+        Indian,         // Hint
+        Japanese,       // Japon
+        Thai,           // Tay
+        Mexican,        // Meksika
+        Spanish,        // İspanyol
+        Mediterranean,  // Akdeniz
+        American,       // Amerikan
+        Other           // Diğer
+    }
+
+    public enum Course
+    {
+        Appetizer,      // Aperatif
+        Soup,           // Çorba
+        Salad,          // Salata
+        MainCourse,     // Ana Yemek
+        SideDish,       // Yan Yemek
+        Dessert,        // Tatlı
+        Beverage,       // İçecek
+        Other           // Diğer Yemekler
+    }
+
+}
